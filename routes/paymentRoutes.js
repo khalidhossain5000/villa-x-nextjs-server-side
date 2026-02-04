931c41f3ffdae06975aec0aa5734d86700957b28
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-04 14:40:21 +0600

    routs added

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,3 +1,7 @@
 import express from 'express'
+import { createStripePaymentIntent } from '../controllers/paymentControllers.js'
 
 const router =express.Router()
+
+
+router.post('/create-payment-intent',createStripePaymentIntent)
