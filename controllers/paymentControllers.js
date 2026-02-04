d7027bba0fcd3071124ed33a9e1a592ee73f69a4
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-04 14:34:14 +0600

    paymenti nten added

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -1,3 +1,11 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+//create payment inter for stripe
+
+export const createStripePaymentIntent=async(req,res)=>{
+    try{
+        const {price} =req.body
+    }
+}
