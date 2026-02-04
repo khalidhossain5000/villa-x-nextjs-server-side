93aed41f161133b0918a7e5d12840b8094aae51d
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-04 14:36:54 +0600

    payment intent created in controoler

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -7,5 +7,20 @@ const stripe = new Stripe(process.env.STRIPE_KEY);
 export const createStripePaymentIntent=async(req,res)=>{
     try{
         const {price} =req.body
+             
+      if (!price || amount < 1) return res.status(400).json({ message: "Invalid amount" });
+      const { client_secret } = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+      })
+
+      console.log('stripe payment intetn sectionrer',client_secret)
+
+      res.send({clientSecret:client_secret})
     }
+    catch (error) {
+    console.error("Stripe payment error:", error);
+    res.status(500).json({ message: "Payment creation failed", error });
+  }
 }
