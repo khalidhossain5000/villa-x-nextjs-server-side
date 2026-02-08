b3e35fec416a3df02c3fd78a6d0a37c5f8e10bf6
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-08 13:08:33 +0600

    get api complted

--- routes/roomCancelRoutes.js
+++ routes/roomCancelRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllRoomCancelRequests, roomCancelRequestByGuest, updateCancelRequestStatus } from '../controllers/roomCancelRequestControolers.js'
+import { getAllRoomCancelRequests, roomCancelRequestByGuest, updateCancelRequestStatus ,getCancelRequestsByUser} from '../controllers/roomCancelRequestControolers.js'
 
 const router=express.Router()
 
@@ -8,4 +8,8 @@ router.post('/room-cancel-request',roomCancelRequestByGuest)
 router.get('/all-room-cancel-requests',getAllRoomCancelRequests)
 
 router.patch('/update-status/:id',updateCancelRequestStatus)
+
+
+router.get('/cancel-requests-by-user/:requestedByEmail',getCancelRequestsByUser)
+
 export default router
