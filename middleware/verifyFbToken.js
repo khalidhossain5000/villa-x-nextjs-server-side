bd6259eb6fcc3c6b8c4c3359c29b300facc8483b
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-08 14:41:21 +0600

    verify user middelware done

--- middleware/verifyFbToken.js
+++ middleware/verifyFbToken.js
@@ -1,14 +1,33 @@
-export const verifyFbToken=async(req,res,next)=>{
-    const authHeader=req.headers.authorization
+const admin = require("firebase-admin");
 
-    const token=authHeader.split(' ')[1]
+const serviceAccount = require("../villa-x-firebase-token-verification.json");
 
-    if(!authHeader || !token) {
-        return res.status(401).send({
-            message:'Unauthorized Access'
-        })
-    }
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export const verifyFbToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log(req, "this is req without decoded");
+  const token = authHeader.split(" ")[1];
+
+  if (!authHeader || !token) {
+    return res.status(401).send({
+      message: "Unauthorized Access",
+    });
+  }
   //verify tooken
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.decoded = decoded;
+    console.log(req, "this is req after decoded initialize",decoded,'this is decoded');
+      next();
+  } catch (error) {
+    console.log(error, "verify error");
+    return res.status(403).send({
+      message: "Forbidden Access",
+      error,
+    });
+  }
 
-    next()
-}
+};
