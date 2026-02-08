fd7bc963d9e3425add632748610c127b05965a58
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-08 16:52:11 +0600

    admin middleware completed added

--- middleware/verifyAdmin.js
+++ middleware/verifyAdmin.js
@@ -1,57 +1,30 @@
+import User from "../models/auth.js";
 
 
+export const verifyAdmin = async (req, res, next) => {
+  console.log(
+    "hello this is req decoded over here",
+    req.decoded,
+    "this decoded from req",
+  );
+  const adminEmail = req.decoded.email;
 
+  try {
+    const user = await User.findOne({ email: adminEmail });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-
-
-
-
-
-// export const verifyAdmin = async (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-  
-//   console.log('hello this is req decoded over here',req.decoded,'this decoded from req')
-
-//   if (!authHeader ) {
-//     return res.status(401).send({
-//       message: "Unauthorized Access",
-//     });
-//   }
-//   const token = authHeader.split(" ")[1];
-
-//   if (!token ) {
-//     return res.status(401).send({
-//       message: "Unauthorized Access",
-//     });
-//   }
-
-
-
-
-//   //verify tooken
-//   try {
-    
-   
-      
-//   } 
-// next();
-// };
-
-
-export const verifyAdmin=async(req,res,next)=>{
-     console.log('hello this is req decoded over here',req.decoded,'this decoded from req')
-     const adminEmail=req.decoded.email
-try{
-    
-}
-
-
-catch (error) {
+    if (user.userRole !== "admin") {
+      return res.status(403).json({ message: "Access denied. Admin only." });
+    }
+    console.log(user, "this is user that we got data", adminEmail);
+    next();
+  } catch (error) {
     console.log(error, "verify admin error");
     return res.status(403).send({
       message: "Forbidden Access",
       error,
     });
   }
-     next()
-}
+};
