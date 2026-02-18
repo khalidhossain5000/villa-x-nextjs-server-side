0f75a47bc64a09c363e686ffa6bb60032ee0a2fb
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-18 16:36:50 +0600

    middlwre aded

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -2,20 +2,21 @@ import express from 'express'
 
 import { verifyFbToken } from '../middleware/verifyFbToken.js'
 import { verifyHost } from '../middleware/verifyHost.js'
+import { verifyAdmin } from '../middleware/verifyAdmin.js'
 import { getAdminStatsData, getGuestStatsData, getHostChartData, getHostStats } from '../controllers/statsControllers.js';
 
 const router=express.Router()
 
 
 
-router.get("/host/:hostEmail", getHostStats);
+router.get("/host/:hostEmail",verifyFbToken,verifyHost, getHostStats);
 
-router.get("/host-chart-data/:hostEmail",getHostChartData)
+router.get("/host-chart-data/:hostEmail",verifyFbToken,verifyHost,getHostChartData)
 
 //admin rotute
-router.get('/admin/stats/:adminEmail',getAdminStatsData)
+router.get('/admin/stats/:adminEmail',verifyFbToken,verifyAdmin,getAdminStatsData)
 
 //guest user data
 
-router.get('/guest-stats/:guestEmail',getGuestStatsData)
+router.get('/guest-stats/:guestEmail',verifyFbToken,getGuestStatsData)
 export default router
